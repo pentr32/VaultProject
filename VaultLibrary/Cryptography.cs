3047a6360d9c01e9e9b64104791cd84bfd05856f
Martin Sonne <Martin Sonne@LAPTOP-JULDTJEK>
2020-08-12 14:20:18 +0200

    Added password encryptions...

--- VaultLibrary/Cryptography.cs
+++ VaultLibrary/Cryptography.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 using VaultLibrary.Model;
 
@@ -8,5 +10,69 @@ namespace VaultLibrary
     public class Cryptography
     {
         public List<StoredUserPassword> AllStoredUserPasswords { get; set; }
+
+        public Cryptography()
+        {
+            AllStoredUserPasswords = new List<StoredUserPassword>();
+        }
+
+        public byte[] GenerateRandomNumber(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+
+        public byte[] EncryptPassword(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateEncryptor(),
+                        CryptoStreamMode.Write);
+
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+        public byte[] DecryptPassword(byte[] dataToDecrypt, byte[] key, byte[] iv)
+        {
+            using (var aes = new AesCryptoServiceProvider())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, aes.CreateDecryptor(),
+                        CryptoStreamMode.Write);
+
+                    cryptoStream.Write(dataToDecrypt, 0, dataToDecrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    var decryptBytes = memoryStream.ToArray();
+
+                    return decryptBytes;
+                }
+            }
+        }
     }
 }
